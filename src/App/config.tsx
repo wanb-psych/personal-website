ea908be51086546712110f3a02f1b74ab687826c
Bin Wan <wanb.psych@outlook.com>
2024-10-08 17:51:29 +0200

    Update config.tsx

--- src/App/config.tsx
+++ src/App/config.tsx
@@ -38,7 +38,7 @@ export const config: Config = {
       display: 'Email',
       aria: 'Send me an email: bin.wan.work@gmail.com',
       icon: <Email />,
-      href: 'mailto:bin.wan.work@gmail.com',
+      href: 'mailto:binwan.academic@gmail.com',
     },
   ],
 };
