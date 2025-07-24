133548460dd6d89ede2b11d75a36ffc6e9efb789
Bin Wan <binwan.academic@gmail.com>
2025-07-25 01:19:25 +0200

    Update Content.tsx

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -42,7 +42,9 @@ export const Content = () => {
         aria-label={config.name.aria}
         title={config.name.aria}
       >
-        {config.name.display}
+        {Array.isArray(config.name.display)
+          ? config.name.display.map((line, idx) => <p key={idx}>{line}</p>)
+          : config.name.display}
       </C.Name>
       <C.Title
         data-v2="title"
@@ -51,7 +53,9 @@ export const Content = () => {
         aria-label={config.title.aria}
         title={config.title.aria}
       >
-        {config.title.display}
+        {Array.isArray(config.title.display)
+          ? config.title.display.map((line, idx) => <p key={idx}>{line}</p>)
+          : config.title.display}
       </C.Title>
     </>
   );
